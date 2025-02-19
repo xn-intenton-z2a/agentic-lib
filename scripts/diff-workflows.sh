9d7db68f54ef149cf44021fdfeb7f2f88dea65cf
Antony at Polycode <antony@polycode.co.uk>
2025-02-19 22:34:01 +0100

    Replace README-EXAMPLE.md with README.md and add sync scripts.
    Replaced `README-EXAMPLE.md` with `README.md` in workflow references to align with consolidated documentation structure. Added `sync-workflows.sh` and `diff-workflows.sh` scripts for managing and comparing workflows between repositories. Introduced `SETUP.md` to guide workflow usage and system setup.

---
+++ scripts/diff-workflows.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+diff .github/workflows/automerge.yml ../repository0/.github/workflows/automerge.yml
+#diff .github/workflows/fix-failing-build.yml ../repository0/.github/workflows/fix-failing-build.yml
+diff .github/workflows/formating.yml ../repository0/.github/workflows/formating.yml
+diff .github/workflows/issue-creator.yml ../repository0/.github/workflows/issue-creator.yml
+diff .github/workflows/issue-reviewer.yml ../repository0/.github/workflows/issue-reviewer.yml
+diff .github/workflows/issue-worker.yml ../repository0/.github/workflows/issue-worker.yml
+diff .github/workflows/publish.yml ../repository0/.github/workflows/publish.yml
+diff .github/workflows/test.yml ../repository0/.github/workflows/test.yml
+diff .github/workflows/update.yml ../repository0/.github/workflows/update.yml
