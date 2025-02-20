50bc5928d785e9321a891c59531cf3d7f0608be9
Antony at Polycode <antony@polycode.co.uk>
2025-02-20 01:18:10 +0100

    Update workflows to use `npm run start` and add release script
    Replaced `node src/lib/main.js` with `npm run start` across workflows for consistency and maintainability. Added `release-versioned-workflows.sh` to facilitate versioned workflow releases. Ensures streamlined updates and better script management.

---
+++ scripts/release-versioned-workflows.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Check for the required tag version argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <tag-version>"
+  exit 1
+fi
+
+TAG_VERSION="$1"
+DEST_DIR="../repository0/.github/workflows"
+
+# Set the appropriate sed in-place flag based on the OS
+if [[ "$(uname)" == "Darwin" ]]; then
+  SED_INPLACE="sed -i ''"
+else
+  SED_INPLACE="sed -i"
+fi
+
+cp -v .github/workflows/automerge.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/automerge.yml"
+
+#cp -v .github/workflows/fix-failing-build.yml "${DEST_DIR}/."
+#$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/fix-failing-build.yml"
+
+cp -v .github/workflows/formating.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/formating.yml"
+
+cp -v .github/workflows/issue-creator.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/issue-creator.yml"
+
+cp -v .github/workflows/issue-reviewer.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/issue-reviewer.yml"
+
+cp -v .github/workflows/issue-worker.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/issue-worker.yml"
+
+cp -v .github/workflows/publish.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/publish.yml"
+
+cp -v .github/workflows/test.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/test.yml"
+
+cp -v .github/workflows/update.yml "${DEST_DIR}/."
+$SED_INPLACE "s/@main/@${TAG_VERSION}/g" "${DEST_DIR}/update.yml"
